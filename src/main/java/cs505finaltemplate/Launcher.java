e7be808dfb2e07f0420015d8a2aa604fa7a4bff9
Julia Stekardis <juliastekardis@gmail.com>
2022-04-24 16:27:14 -0400

    get graph db engine working

--- src/main/java/cs505finaltemplate/Launcher.java
+++ src/main/java/cs505finaltemplate/Launcher.java
@@ -36,7 +36,7 @@ public class Launcher {
         //startig DB/CEP init
 
         //READ CLASS COMMENTS BEFORE USING
-        //graphDBEngine = new GraphDBEngine();
+        graphDBEngine = new GraphDBEngine();
 
         cepEngine = new CEPEngine();
 
